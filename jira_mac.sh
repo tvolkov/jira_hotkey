68136a772c7d2a666e171ac6a70d613750618af8
Timofey Volkov <timofey.volkov@gmail.com>
2019-06-24 10:57:19 +0100

    add readme

--- jira_mac.sh
+++ jira_mac.sh
@@ -1,4 +1,14 @@
 #!/bin/sh
+# how to get it working on macOS:
+# 1. hit CMD+Space and search for 'Automator'
+# 2. Once Automator is open cick 'Quick Action'
+# 3. for 'Workflow receives current' choose 'no input'
+# 4. from the 'Actions' pane select 'Run Shell Script' and drag-n-drop it to the right pane
+# 5. put the absolute path to this script to the text field
+# 6. Save it and close Automator
+# 7. goto System Preferences -> Keyboard -> Shortcuts -> Services and find the service you've just saved
+# 8. dbl-click on the right of it and press the desired keystroke. Close the preferences windoe
+# 9. have fun!
 
 if [ -z "$JIRA_URL" ]; then
     JIRA_URL="https://jira.global.tesco.org/browse/PDR-"
